79204f522459cc8020f696af7408381f4fbf9701
otaviocals <otaviolcals@gmail.com>
2016-05-17 23:43:43 -0300

    created github project

---
+++ config/initializers/assets.rb
@@ -0,0 +1,29 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+
+Rails.application.config.assets.precompile += %w( home.css )
+
+Rails.application.config.assets.precompile += %w( login.css )
+Rails.application.config.assets.precompile += %w( login_fb.css )
+Rails.application.config.assets.precompile += %w( login_gp.css )
+Rails.application.config.assets.precompile += %w( cad_1.css )
+Rails.application.config.assets.precompile += %w( cad_2.css )
+Rails.application.config.assets.precompile += %w( cad_3_stud.css )
+Rails.application.config.assets.precompile += %w( cad_3_teach.css )
+Rails.application.config.assets.precompile += %w( cad_3_work.css )
+Rails.application.config.assets.precompile += %w( cad_3_out.css )
+Rails.application.config.assets.precompile += %w( cad_4.css )
+Rails.application.config.assets.precompile += %w( menu.css )
+Rails.application.config.assets.precompile += %w( how_to.css )
+Rails.application.config.assets.precompile += %w( point_check.css )
+Rails.application.config.assets.precompile += %w( redeem.css )
+Rails.application.config.assets.precompile += %w( join.css )
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
---
+++ config/routes.rb
@@ -0,0 +1,74 @@
+Rails.application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  root 'home#home'
+
+  get 'login' => 'home#login'
+  get 'login_fb' => 'home#login_fb'
+  get 'login_gp' => 'home#login_gp'
+
+  get 'cad_1' => 'home#cad_1'
+  get 'cad_2' => 'home#cad_2'
+  get 'cad_3_stud' => 'home#cad_3_stud'
+  get 'cad_3_teach' => 'home#cad_3_teach'
+  get 'cad_3_work' => 'home#cad_3_work'
+  get 'cad_3_out' => 'home#cad_3_out'
+  get 'cad_4' => 'home#cad_4'
+  get 'menu' => 'home#menu'
+  get 'point_check' => 'home#point_check'
+  get 'how_to' => 'home#how_to'
+  get 'redeem' => 'home#redeem'
+  get 'join' => 'home#join'
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+  # Example of named route that can be invoked with purchase_url(id: product.id)
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+  # Example resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Example resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Example resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Example resource route with more complex sub-resources:
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', on: :collection
+  #     end
+  #   end
+
+  # Example resource route with concerns:
+  #   concern :toggleable do
+  #     post 'toggle'
+  #   end
+  #   resources :posts, concerns: :toggleable
+  #   resources :photos, concerns: :toggleable
+
+  # Example resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+end
